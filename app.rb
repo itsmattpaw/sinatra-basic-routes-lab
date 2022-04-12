66df5aaf795cb7c09c320a6bf5985c78566c4582
itsmattpaw <itsmattpaw@gmail.com>
2022-04-12 18:19:20 -0400

    commit tree ac28bdcb2eab565c8029645f371062204a0af6fb

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Home Page"
+    end
+    get '/name' do
+        "My name is Matt"
+    end
+    get '/hometown' do
+        "My hometown is Livonia"
+    end
+    get '/favorite-song' do
+        "My favorite song is __"
+    end
 end
